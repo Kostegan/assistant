a144a0f599c4b59a8f7981c8cc446fee629d70d4
Kostegan <churilovkonstantin@gmail.com>
2017-02-12 14:37:46 +0400

    added Main class

--- src/main/java/com/assistant/Main.java
+++ src/main/java/com/assistant/Main.java
@@ -1,8 +1,35 @@
 package com.assistant;
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
+    public static String file = "C:\\java_work_github\\2017work\\assistant-app\\src\\main\\resources\\text.txt";
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        read(file);
+    }
+
+    public static void read(String file) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
